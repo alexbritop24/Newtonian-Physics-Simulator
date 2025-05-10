4b15c7ff035ccd774a75d98f5d40bc0990a4f179
Alex Brito <alexbrito@myyahoo.com>
2025-05-09 23:12:10 -0400

    Add touch-to-spawn interaction in GameScene

---
+++ Newtonian Physics Simulator/Newtonian Physics SimulatorTests/Newtonian_Physics_SimulatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Newtonian_Physics_SimulatorTests.swift
+//  Newtonian Physics SimulatorTests
+//
+//  Created by Alex Brito on 5/9/25.
+//
+
+import Testing
+@testable import Newtonian_Physics_Simulator
+
+struct Newtonian_Physics_SimulatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
